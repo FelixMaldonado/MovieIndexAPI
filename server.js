6c5829c54404782d4373f5ff40cf62b206f6a4a6
Felix Maldonado <felix_maldonado@hotmail.com>
2019-11-16 13:16:46 -0500

    fixed listen

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use((error, req, res, next) =>{
 
 const PORT = process.env.PORT || 8000
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
 
